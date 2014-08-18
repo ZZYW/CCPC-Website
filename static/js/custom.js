3ca60ee40d35cd0e4d7799747d7a6eaa4486f86e
jane363636 <jane363636@gmail.com>
2014-08-18 14:38:16 -0400

    img list fixed

--- static/js/custom.js
+++ static/js/custom.js
@@ -5,7 +5,7 @@
 
 
 // agenda tabs and highlighted speakers tabs
-$('#myTab a').click(function (e) {
+$('.myTab a').click(function (e) {
     e.preventDefault();
     $(this).tab('show');
 });
@@ -18,18 +18,18 @@ $(".team-member").tooltip(
 
 
 //smooth scrolling js code
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
+//$(function() {
+//  $('a[href*=#]:not([href=#])').click(function() {
+//    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+//      var target = $(this.hash);
+//      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+//      if (target.length) {
+//        $('html,body').animate({
+//          scrollTop: target.offset().top
+//        }, 1000);
+//        return false;
+//      }
+//    }
+//  });
+//});
 //smooth js ends here
