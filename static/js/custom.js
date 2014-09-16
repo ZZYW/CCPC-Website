d0020cca6e16f0944e5a9add3f2eefe94cfbc7d7
nycucumber <yang.wang.dec@gmail.com>
2014-09-16 14:17:31 -0400

    credits added

--- static/js/custom.js
+++ static/js/custom.js
@@ -70,7 +70,7 @@ $(document).ready(function () {
 		}
 	});
 
-
+	$('body').css('height', $('#theBottom').offset().top);
 });
 
 
@@ -140,30 +140,3 @@ function animateWhenReachIn(selector, effect, duration, offset, delay) {
 		}
 	})
 };
-
-
-vimeoEmbedder = function () {
-
-	$("#video").each(function () {
-
-		var videoObj = $(this),
-			videoContainer = $(".video-container", this),
-			placeholderImg = videoObj.find(".placeholder"),
-			videoColour = "aaaaaa";
-
-		if ((placeholderImg.attr("height") / placeholderImg.attr("width")) > .57) $(".video-container", this).addClass("fourbythree");
-
-		if (videoObj.attr("vimeo-color")) videoColour = videoObj.attr("vimeo-color");
-
-		var videoHTML = '<iframe src="http://player.vimeo.com/video/' + videoObj.attr("vimeo-id") + '?title=0&amp;byline=0&amp;portrait=0&amp;color=' + videoColour + '&amp;autoplay=1&amp;api=1" width="' + placeholderImg.attr("width") + '" height="' + placeholderImg.attr("height") + '" width="' + placeholderImg.attr("width") + '" height="' + placeholderImg.attr("height") + '" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>';
-
-		$(".play", this).on("click", function (e) {
-			videoContainer.empty().append(videoHTML);
-		});
-
-	});
-
-	if ($("body").hasClass("home")) $("html").addClass("hideScroll");
-	else $("html").removeClass("hideScroll");
-
-}
